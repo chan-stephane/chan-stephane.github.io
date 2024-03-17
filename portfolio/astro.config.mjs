6134de4005d7f29919a95f2b6b91c73fc05c535b
Stéphane <chkstephane32@gmail.com>
2024-03-17 13:49:01 +0300

    [Fix] fxix some bugs

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -3,12 +3,8 @@ import compress from "astro-compress";
 import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), compress()],
   site: 'https://chan-stephane.github.io',
-  output: "server",
-  adapter: vercel()
 });
